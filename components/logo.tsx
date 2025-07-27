e71b108dee3cd1bd899c31631131fb11168fe439
Sophia <sophiagg712@gmail.com>
2025-07-28 00:18:37 +1200

    Test with regular img tags instead of Next.js Image component

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import Image from "next/image"
 
 interface LogoProps {
   className?: string
@@ -9,13 +8,10 @@ export default function Logo({ className = "" }: LogoProps) {
   return (
     <Link href="/" className={`block ${className}`}>
       <div className="relative h-44 w-auto">
-        <Image
+        <img
           src="/images/logo.png"
           alt="Millars Beach Conservation Trust Logo"
-          width={180}
-          height={180}
           className="h-full w-auto object-contain"
-          priority
         />
       </div>
     </Link>
